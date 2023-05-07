1f84664c2477fc8d9d26c180a55b614f93c2ae0b
amir <amirhosseinmohsenifard@gmail.com>
2023-05-07 17:58:48 +0330

    make the blog urls SEO-Friendly and modifying reverse urls

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
@@ -16,7 +17,7 @@ class Post(models.Model):
         
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts', default=1)
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     body = models.TextField()
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
     publish = models.DateTimeField(timezone.now())
@@ -34,4 +35,10 @@ class Post(models.Model):
     
     def __str__(self):
         return f"{self.id} - {self.title}"
+    
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", args=[self.publish.year,
+                                                 self.publish.month,
+                                                 self.publish.day,
+                                                 self.slug])
     
